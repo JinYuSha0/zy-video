eb9fadf2707df1622f77bec69524eb7d431248ff
邵瑾瑜 <1009943858@qq.com>
2018-05-08 21:23:27 +0800

    first commit

---
+++ app/src/components/header/header.js
@@ -0,0 +1,82 @@
+import './header.less'
+
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { jump } from '../../util/util'
+import { ipcRenderer } from 'electron'
+import fs from 'fs'
+
+class Header extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isMax: false
+        }
+    }
+
+    onMinClick = () => {
+        ipcRenderer.send('minimize-window')
+    }
+
+    onMaxClick = () => {
+        ipcRenderer.send('maximize-window')
+        this.setState({ isMax: true })
+    }
+
+    onUnMaxClick = () => {
+        ipcRenderer.send('unmaximize-window')
+        this.setState({ isMax: false })
+    }
+
+    onCloseClick = () => {
+        ipcRenderer.send('close-all-window')
+    }
+
+    onLoginClick = () => {
+        fs.writeFileSync('./modal.html', "11")
+    }
+
+    render() {
+        const { isMax } = this.state
+        const { user } = this.props
+        return (
+            <header className="layoutHeader">
+                <div className="headerLogo" onClick={() => {jump('/')}}>
+                    <span className="logo"/>
+                    <h2>中宜视频系统</h2>
+                </div>
+
+                <div className="headerController">
+                    <div className="barWrapper">
+                        <div className="user" onClick={this.onLoginClick}>
+                            <span className="avatar"></span>
+                            <p className="userName">{user.get('isLogin') ? user.getIn(['userInfo', 'nickName']) : '未登录'}</p>
+                        </div>
+
+                        <span className="icon settings"/>
+                    </div>
+
+                    <span className="divider"></span>
+
+                    <div className="controllerWrapper">
+                        <span className="icon min" onClick={this.onMinClick}/>
+
+                        {
+                            isMax ? <span className="icon unmax" onClick={this.onUnMaxClick}/> :
+                                <span className="icon max" onClick={this.onMaxClick}/>
+                        }
+
+                        <span className="icon close" onClick={this.onCloseClick}/>
+                    </div>
+                </div>
+            </header>
+        )
+    }
+}
+
+export default connect(
+    ({user}) => ({user}),
+    (dispatch) => bindActionCreators({
+    }, dispatch)
+)(Header)
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const path = require('path')
+const webpack = require('webpack')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+process.env.NODE_ENV = 'development'
+
+module.exports = {
+    watch: true,
+    target: 'electron-main',
+    entry: [
+        './app/src/index.js'
+    ],
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, './app/dist'),
+        publicPath: path.join(__dirname, './app/src')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use:[
+                    {loader: 'style-loader'},
+                    {loader: 'css-loader', options: {module: false}},
+                ]
+            },
+            {
+                test: /\.less$/,
+                use:[
+                    {loader: 'style-loader'},
+                    {loader: 'css-loader', options:{module: false}},
+                    {loader: 'less-loader'}
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                use: {loader: "url-loader", options: {limit: 50000}}
+            },
+            {
+                test: /\.jsx?$/,
+                use: {loader: 'babel-loader'},
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(['./app/dist']),
+    ],
+    resolve: {
+        extensions: ['.js', '.json', '.jsx']
+    }
+}
